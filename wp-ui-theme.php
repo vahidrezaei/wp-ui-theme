eaca3d2c5e1cf5549f5487467990326e686a37ef
vahidrezaei.sh <vahidrezaei.sh@gmail.com>
2023-05-27 18:58:28 +0330

    Initial commit

---
+++ wp-ui-theme.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Wp Ui Theme
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: A brief description of the Plugin.
+Version: 1.0
+Author: ya-ali
+Author URI: http://URI_Of_The_Plugin_Author
+License: A "Slug" license name e.g. GPL2
+*/
